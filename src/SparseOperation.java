94cfa65dca7e72a4a42e892b1e8c039fc978b699
Edouard360 <mailedouardmehlman@gmail.com>
2017-02-04 19:15:52 +0100

    Adding comments and expliciting names

--- src/SparseOperation.java
+++ src/SparseOperation.java
@@ -1,11 +1,20 @@
-import java.util.ArrayList;
 import java.util.TreeSet;
 
+/**
+ * The SparseOperation class.
+ * 
+ * Only one static method used in SparseRepresentation for the substration of
+ * the columns in the "reduce" function (that corresponds to the reduction
+ * algorithm).
+ */
+
 public class SparseOperation {
-	static void substract(TreeSet<Integer> arrayList,TreeSet<Integer> toSubstract){
-		for(Integer i :toSubstract){
-			if(!arrayList.remove(i)){arrayList.add(i);};
+	static void substract(TreeSet<Integer> arrayList, TreeSet<Integer> toSubstract) {
+		for (Integer i : toSubstract) {
+			if (!arrayList.remove(i)) {
+				arrayList.add(i);
+			}
 		}
-		return ;
+		return;
 	}
 }
--- src/SparseRepresentation.java
+++ src/SparseRepresentation.java
@@ -1,97 +1,125 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.TreeSet;
 import java.util.Vector;
 
+/**
+ * The SparseRepresentation class.
+ * 
+ * This class has two constructor so as to be lenient with regards to
+ * initialization: Initialize either with a vector of simplices or with a
+ * Filtration object.
+ * 
+ * This class is mainly responsible of the reduction of the matrix via the
+ * "reduce" method.
+ * 
+ * Then it is possible to "plot" the matrix using the toString method.
+ */
+
 public class SparseRepresentation {
 	ArrayList<TreeSet<Integer>> matrix;
-	
-	SparseRepresentation(Vector<Simplex> vectorSimplex){
+
+	SparseRepresentation(Vector<Simplex> vectorSimplex) {
+		/*
+		 * Calls the constructor below with the filtration correponding to
+		 * vectorSimplex.
+		 */
 		this(new Filtration(vectorSimplex));
 	}
-	SparseRepresentation(Filtration F){
-		
+
+	SparseRepresentation(Filtration F) {
+		/*
+		 * Calls the constructor below with the filtration correponding to
+		 * vectorSimplex.
+		 */
 		matrix = new ArrayList<TreeSet<Integer>>();
-		
-		// Sorting the filtration F
-		F.sort();
-		
-		// Set the order of each simplex in the filtration
-		F.computeOrder();
-		
 		matrix = F.getSparseRepresentation();
 	}
-	
-	public ArrayList<TreeSet<Integer>> transpose(){
-		ArrayList<TreeSet<Integer>> tmatrix = new ArrayList<TreeSet<Integer>>(matrix.size());
-		for(int i=0;i<matrix.size();i++){
-			tmatrix.add(new TreeSet<Integer>());
-		}
-		int i = 0;
-		for(TreeSet<Integer> orderList:matrix){
-			for(Integer order:orderList){
-				tmatrix.get(order).add(i);
-			}
-			i++;
-		}
-		return tmatrix;
-	
-	}
-	
-	public ArrayList<Integer> reduce(){
-		ArrayList<Integer> lowIndices = new ArrayList<Integer>();
-		HashMap<Integer,TreeSet<Integer>> lowMapToColumns= new HashMap<Integer,TreeSet<Integer>>();
-		int j = 0,lowerIndex=-1;
+
+	public ArrayList<Integer> reduce() {
+		/*
+		 * The reduce method corresponding to the reduction algorithm. A column
+		 * is represented by a TreeSet. Once we have our matrix, we use the
+		 * reduction algorithm, keeping track of the lowest indices, and calling
+		 * SparseOperation.substract for the operations on columns. NB: the
+		 * lowMapToColumns hashMap enables fast access to the TreeSet in O(1).
+		 */
+		ArrayList<Integer> lowestIndices = new ArrayList<Integer>();
+		HashMap<Integer, TreeSet<Integer>> lowMapToColumns = new HashMap<Integer, TreeSet<Integer>>();
+		int j = 0, lowerIndex = -1;
 
 		System.out.println(matrix.size());
-		for(TreeSet<Integer> listOrder: matrix){
-			while(true){
-				if(listOrder.size()==0){
+		for (TreeSet<Integer> listOrder : matrix) {
+			while (true) {
+				if (listOrder.size() == 0) {
 					lowerIndex = -1;
 					break;
 				}
 				lowerIndex = listOrder.last();
 				TreeSet<Integer> toSubstract = lowMapToColumns.get(lowerIndex);
-				if(toSubstract != null){
+				if (toSubstract != null) {
 					SparseOperation.substract(listOrder, toSubstract);
-				}else{
+				} else {
 					break;
 				}
 			}
-			lowIndices.add(lowerIndex);
-			if(lowerIndex!=-1){
+			lowestIndices.add(lowerIndex);
+			if (lowerIndex != -1) {
 				lowMapToColumns.put(lowerIndex, matrix.get(j));
 			}
-			
+
 			j++;
 		}
-		return(lowIndices);
+		return (lowestIndices);
 	}
-		
-	public String toString(){
+
+	public ArrayList<TreeSet<Integer>> transpose() {
+		/*
+		 * The transpose method. Does what it suggest. However, it is only used
+		 * for debugging purpose. Keep in mind that the chosen representation of
+		 * our original matrix is column by column and not line by line. This
+		 * transpose the matrix and enables an easy plot of it.
+		 */
+		ArrayList<TreeSet<Integer>> tmatrix = new ArrayList<TreeSet<Integer>>(matrix.size());
+		for (int i = 0; i < matrix.size(); i++) {
+			tmatrix.add(new TreeSet<Integer>());
+		}
+		int i = 0;
+		for (TreeSet<Integer> orderList : matrix) {
+			for (Integer order : orderList) {
+				tmatrix.get(order).add(i);
+			}
+			i++;
+		}
+		return tmatrix;
+
+	}
+
+	public String toString() {
+		/*
+		 * Only used for debugging purpose. Provides a nice print of the object.
+		 */
 		int size = matrix.size();
 		String s = "   ";
-		for(int j = 0;j<size;j++){
-			s+=" "+Integer.toString(j);
+		for (int j = 0; j < size; j++) {
+			s += " " + Integer.toString(j);
 		}
 		s += "\n   ";
-		for(int j = 0;j<size;j++){
-			s+=" |";
+		for (int j = 0; j < size; j++) {
+			s += " |";
 		}
 		s += "\n";
 		ArrayList<TreeSet<Integer>> transpose = this.transpose();
-		int j =0;
-		for(TreeSet<Integer> list: transpose){
-			s += Integer.toString(j)+" - ";
-			for(int i = 0;i<size;i++){
-				s+=list.contains(i)?"1":"0";
-				s+=(i+1!=size)?" ":"\n";
+		int j = 0;
+		for (TreeSet<Integer> list : transpose) {
+			s += Integer.toString(j) + " - ";
+			for (int i = 0; i < size; i++) {
+				s += list.contains(i) ? "1" : "0";
+				s += (i + 1 != size) ? " " : "\n";
 			}
 			j++;
 		}
-		
+
 		return s;
-	 }
+	}
 }
