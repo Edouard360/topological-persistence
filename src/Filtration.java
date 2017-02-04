b9346ed56ca7126afd74a6c6f28537da31386fe8
Edouard360 <mailedouardmehlman@gmail.com>
2017-02-04 18:37:09 +0100

    Adding comments and expliciting names

--- src/Filtration.java
+++ src/Filtration.java
@@ -6,118 +6,148 @@ import java.util.HashMap;
 import java.util.TreeSet;
 import java.util.Vector;
 
-public class Filtration {
-	Vector<Simplex> F;
-	HashMap<TreeSet<Integer>,Simplex> H;
-	HashMap<Integer,Simplex> orderToSimplex;
+/**
+ * The Filtration class.
+ * 
+ * This class has a single constructor, which take a vector of simplices, sort
+ * it (see Simplex class), and then set an order attribute to the simplices
+ * according to that sort. At the same time, it creates two HashMap highly
+ * useful for optimization.
+ * 
+ * The filtration object has a getSparseRepresentation method, which creates the
+ * sparse matrix column by column, looking at the derivative of each simplex.
+ * 
+ */
 
-	Filtration(Vector<Simplex> F){
-		this.F = F;
-		H = new HashMap<TreeSet<Integer>,Simplex>();
-		orderToSimplex = new HashMap<Integer,Simplex>();
-	}
-	
-	public Vector<Simplex> get(){
-		return F;
-	}
-	
-	public String toString(){
-		String s = "";
-		for(Simplex simplex:F){
-			s+=simplex.toString();
-		}
-		return s;
-	}
-	
-	public String toStringSimple(){
-		String s = "";
-		for(Simplex simplex:F){
-			s+=simplex.toStringSimple();
-		}
-		return s;
-	}
-	
-	public String getIntervals(TreeSet<Interval> t){
-		/*
-		 * For debugging purpose, or simple (=short) filtrations.
-		 * Indeed, operating on a long string is heavy, and therefore
-		 * the function scales poorly
-		 */
-		String s ="";
-		for(Interval interval:t){
-			Simplex begin = orderToSimplex.get(interval.begin);
-			s+=Integer.toString(begin.dim)+" "+Float.toString(begin.val)+" ";
-			if(interval.end!= -1){
-				s+=Float.toString(orderToSimplex.get(interval.end).val);
-			}else{
-				s+="inf";
+public class Filtration {
+	Vector<Simplex> simplices;
+	HashMap<TreeSet<Integer>, Simplex> treeSetToSimplex;
+	HashMap<Integer, Simplex> orderToSimplex;
+
+	Filtration(Vector<Simplex> simplices) {
+		this.simplices = simplices;
+		treeSetToSimplex = new HashMap<TreeSet<Integer>, Simplex>();
+		orderToSimplex = new HashMap<Integer, Simplex>();
+
+		// Sorting the simplices vector
+		Collections.sort(simplices, new Comparator<Simplex>() {
+			public int compare(Simplex si, Simplex sj) {
+				return si.compareTo(sj);
 			}
-			s+="\n";
+		});
+
+		// Set the attribute named `order` for each simplex, and fill the two
+		// hashMap
+		int j = 0;
+		for (Simplex s : simplices) {
+			// NB: Insted of updating the val attribute (like in the course), we
+			// simply compute a order attribute
+			// which accounts for th order of the filtration.
+			s.order = j++;
+			treeSetToSimplex.put(s.getTreeSet(), s);
+			orderToSimplex.put(s.order, s);
 		}
-		s = s.substring(0, s.length()-1); // To remove the last "\n"
-		return s;
 	}
-	
-	public void printIntervalsToFile(TreeSet<Interval> t,PrintWriter out){
+
+	public ArrayList<TreeSet<Integer>> getSparseRepresentation() {
 		/*
-		 * Prints the intervals to the specified output.
-		 * Probably many I/Os here, but this method
-		 * scales far better than the one above.
+		 * The getSparseRepresentation method. Returns a sparseMatrix
+		 * representation of our filtration. For each simplex (remember that
+		 * they are already ordered), it computes the derivative of that simplex
+		 * and uses the getOrderList method to return the correponding indexes.
 		 */
-		for(Interval interval:t){
+		ArrayList<TreeSet<Integer>> sparseMatrix = new ArrayList<TreeSet<Integer>>();
+		for (Simplex s : simplices) {
+			ArrayList<TreeSet<Integer>> listSimplices = s.derivate();
+			sparseMatrix.add(this.getOrderList(listSimplices));
+		}
+		return sparseMatrix;
+	}
+
+	public TreeSet<Integer> getOrderList(ArrayList<TreeSet<Integer>> listSimplices) {
+		/*
+		 * The getOrderList method. Instead of a for loop in O(n), on the
+		 * Vector<Simplex> F, using a hashMap O(1) to find the Simplex
+		 * corresponding to the TreeSet...
+		 */
+		TreeSet<Integer> orderList = new TreeSet<Integer>();
+		for (TreeSet<Integer> t : listSimplices) {
+			if (!t.isEmpty()) {
+				orderList.add(treeSetToSimplex.get(t).order);
+			}
+		}
+		return orderList;
+	}
+
+	public void printIntervalsToFile(TreeSet<Interval> t, PrintWriter out) {
+		/*
+		 * The printIntervalsToFile method, that prints the intervals to the
+		 * specified output. It is the translation back to the real floats of
+		 * the filtration ! The orderToSimplex HashMap guarantees O(1) to find
+		 * the right Simplex. NB: Though there are many I/Os here, this method
+		 * scales pretty well.
+		 */
+		for (Interval interval : t) {
 			Simplex begin = orderToSimplex.get(interval.begin);
-			out.print(Integer.toString(begin.dim)+" "+Float.toString(begin.val)+" ");
-			if(interval.end!= -1){
+			out.print(Integer.toString(begin.dim) + " " + Float.toString(begin.val) + " ");
+			if (interval.end != -1) {
 				out.print(Float.toString(orderToSimplex.get(interval.end).val));
-			}else{
+			} else {
 				out.print("inf");
 			}
 			out.println("");
 		}
 	}
-	
-	public void sort(){
-		Collections.sort(F, new Comparator<Simplex>() {
-			public int compare(Simplex si, Simplex sj) {
-				return si.compare(sj);
+
+	public int getSize() {
+		/*
+		 * Used only for debugging purpose.
+		 */
+		return simplices.size();
+	}
+
+	public String toString() {
+		/*
+		 * Prints all the simplices in the filtration object. Used only for
+		 * debugging purpose.
+		 */
+		String s = "";
+		for (Simplex simplex : simplices) {
+			s += simplex.toString();
+		}
+		return s;
+	}
+
+	public String toStringSimple() {
+		/*
+		 * Prints a bit more nicely all the simplices in the filtration object.
+		 * Used only for debugging purpose.
+		 */
+		String s = "";
+		for (Simplex simplex : simplices) {
+			s += simplex.toStringSimple();
+		}
+		return s;
+	}
+
+	public String getIntervals(TreeSet<Interval> t) {
+		/*
+		 * Translate back to the real intervals, and returns the string. For
+		 * debugging purpose, or simple (=short) filtrations. Indeed, operating
+		 * on a long string is heavy, and therefore the function scales poorly
+		 */
+		String s = "";
+		for (Interval interval : t) {
+			Simplex begin = orderToSimplex.get(interval.begin);
+			s += Integer.toString(begin.dim) + " " + Float.toString(begin.val) + " ";
+			if (interval.end != -1) {
+				s += Float.toString(orderToSimplex.get(interval.end).val);
+			} else {
+				s += "inf";
 			}
-			});
-	}
-	
-	public void computeOrder(){
-		/*
-		 * Insted of updating the val attribute (like in the course),
-		 * we simply compute a order attribute which accounts for the
-		 * order of the filtration
-		 */
-		int j = 0;
-		for(Simplex s:F){
-			s.order = j++;
-			H.put(s.getTreeSet(), s);
-			orderToSimplex.put(s.order, s);
+			s += "\n";
 		}
-	}
-	
-	public TreeSet<Integer> getOrderList(ArrayList<TreeSet<Integer>> listSimplices){
-		/*
-		 * Instead of a for loop in O(n), on the Vector<Simplex> F, using a hashMap
-		 * O(1) to find the derivatives...
-		 */
-		TreeSet<Integer> orderList = new TreeSet<Integer>();
-		for(TreeSet<Integer> t : listSimplices){
-			if(!t.isEmpty()){
-				orderList.add(H.get(t).order);
-			}			
-		}
-		return orderList;
-	}
-	
-	public ArrayList<TreeSet<Integer>> getSparseRepresentation(){
-		ArrayList<TreeSet<Integer>> sparseMatrix = new ArrayList<TreeSet<Integer>>();
-		for(Simplex s : F){
-			ArrayList<TreeSet<Integer>> listSimplices = s.derivate();
-			sparseMatrix.add(this.getOrderList(listSimplices));
-		}
-		return sparseMatrix;
+		s = s.substring(0, s.length() - 1); // To remove the last "\n"
+		return s;
 	}
 }
